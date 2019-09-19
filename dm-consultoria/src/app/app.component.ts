f9d9b8e93510bb2e9d4b075666374844a151d128
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2019-09-19 11:48:06 -0300

    Inicio do Desafio DM Consultoria (Angular)

---
+++ dm-consultoria/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dm-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'dm-consultoria';
+}
