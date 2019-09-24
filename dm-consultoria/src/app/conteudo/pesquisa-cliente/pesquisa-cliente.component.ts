ad55803703c99e603cd9d05566e81eb7d436f45f
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2019-09-24 00:38:20 -0300

    criacao da classe cliente e lista de clientes

--- dm-consultoria/src/app/conteudo/pesquisa-cliente/pesquisa-cliente.component.ts
+++ dm-consultoria/src/app/conteudo/pesquisa-cliente/pesquisa-cliente.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Cliente } from 'src/app/model/cliente.model';
 
 @Component({
   selector: 'dm-pesquisa-cliente',
@@ -6,6 +7,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pesquisa-cliente.component.css']
 })
 export class PesquisaClienteComponent implements OnInit {
+
+  @Input() cliente: Cliente
+
+  clientes: Cliente[] = [  
+    {  
+      id:1,
+      nome:"Paulo Santos de Almeida",
+      email:"paulo.santos@email.com",
+      cpf:"02341233409"
+    },
+    {  
+      id:2,
+      nome:"Antonio Marques da Silva",
+      email:"tonhomsilva@email.com",
+      cpf:"23456789012"
+    },
+    {  
+      id:3,
+      nome:"João Sousa Siqueira",
+      email:"jaosiq@email.com",
+      cpf:"34567890123"
+    },
+    {  
+      id:4,
+      nome:"Armando Jogada",
+      email:"armando.player@gmail.com",
+      cpf:"45678901234"
+    },
+    {  
+      id:5,
+      nome:"Jordência Sexta",
+      email:"jordanfriday@hotmail.com",
+      cpf:"56789012345"
+    },
+    {  
+      id:6,
+      nome:"Kelly Guissa",
+      email:"salsich@obig.net",
+      cpf:"67890123456"
+    },
+    {  
+      id:7,
+      nome:"Alexandre R. Ferreira",
+      email:"xanderifer1974@gmail.com",
+      cpf:"04436823725"
+    }
+  ]
 
   constructor() { }
 
---
+++ dm-consultoria/src/app/model/cliente.model.ts
@@ -0,0 +1,9 @@
+
+export class Cliente {
+
+    id:number;
+    nome:string;
+    email:string;
+    cpf:string;  
+
+}
