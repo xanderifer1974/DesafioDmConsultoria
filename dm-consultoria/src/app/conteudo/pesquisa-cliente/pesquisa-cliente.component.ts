d0b8f349b86b386979e7358ddecbadd07b0ff884
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2019-09-25 18:00:33 -0300

    Utilizando o pipe async no get do service

--- dm-consultoria/src/app/conteudo/pesquisa-cliente/pesquisa-cliente.component.ts
+++ dm-consultoria/src/app/conteudo/pesquisa-cliente/pesquisa-cliente.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Cliente } from 'src/app/model/cliente.model';
 import { ClienteService } from 'src/app/services/cliente.service';
+import { Observable } from 'rxjs';
 
 
 @Component({
@@ -12,14 +13,19 @@ import { ClienteService } from 'src/app/services/cliente.service';
 export class PesquisaClienteComponent implements OnInit {
 
    clientes: Cliente []
+
+   clientes$: Observable<Cliente[]>;
   
 
   constructor(private clienteService:ClienteService) { }
 
   ngOnInit() {    
 
-     this.clienteService.getClientes()
-    .subscribe(data => this.clientes = data);
+    //  this.clienteService.getClientes()
+    // .subscribe(data => this.clientes = data);
+
+    this.clientes$ = this.clienteService.getClientes();
+
   }
 
 }
